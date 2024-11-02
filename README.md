ee7b7122ed759c6563e8e5ede8975039042e303b
AhabCry <jr.camacho296@gmail.com>
2024-11-01 23:36:37 -0500

    Adicion del dulce mas vendido

---
+++ README.md
@@ -0,0 +1,2 @@
+# RetoProgramacion
+# RetoProgramacion
--- venv/RetoProgramacion.py
+++ venv/RetoProgramacion.py
@@ -11,22 +11,37 @@ articulos = [
 
 def ingresar_compras():
     lista_compras = []
-    respuesta = "Si"
-    while respuesta == "Si":
+    respuesta = "SI"
+    while respuesta == "SI":
         nombre = input("Ingrese el nombre del comprador: ")
         fecha = input("Ingrese la fecha de la compra (Año-Mes-Dia): ")
         compras = []
-        compra = "Si"
-        while compra == "Si":
+        compra = "SI"
+        while compra == "SI":
             codigo = input("Ingrese el código del producto: ")
             cantidad = int(input("Ingrese la cantidad comprada del producto: "))
             compras.append({"Codigo": codigo, "Cantidad": cantidad})
-            compra = input("¿Quiere ingresar otro producto? (Si/No): ")
+            compra = input("¿Quiere ingresar otro producto? (Si/No): ").upper()
         lista_compras.append({"nombre": nombre, "fecha": fecha, "compras": compras})
-        respuesta = input("¿Quiere ingresar otra compra? (Si/No): ")
+        respuesta = input("¿Quiere ingresar otra compra? (Si/No): ").upper()
     return lista_compras
 
 lista_compras = ingresar_compras()
+
+cant = {}
+
+for compra_cliente in lista_compras:
+    for compra in compra_cliente["compras"]:
+        codigo = compra["Codigo"]
+        cantidad = compra["Cantidad"]
+
+        if codigo in cant:
+            cant[codigo] += cantidad
+        else:
+            cant[codigo] = cantidad
+
+producto_mas_vendido = max(cant, key=cant.get)
+cantidad_mas_vendida = cant[producto_mas_vendido]
 
 lista_compras_ordenada = sorted(lista_compras, key=lambda x: x["fecha"])
 
@@ -35,3 +50,5 @@ for compra_cliente in lista_compras_ordenada:
     print(f"Fecha: {compra_cliente['fecha']}")
     for compra in compra_cliente['compras']:
         print(f"  Código: {compra['Codigo']}, Cantidad: {compra['Cantidad']}")
+
+print(f"El producto mas vendido es el que tiene codigo {producto_mas_vendido} con {cantidad_mas_vendida} unidades totales")
